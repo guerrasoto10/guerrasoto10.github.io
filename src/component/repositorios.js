ab29057b594001a4e13a61b31c00815bc18d267f
Gustavo Guerra <gaguerra84@gmail.com>
2020-10-11 20:13:57 -0400

    Aplicando sessionStorage

--- src/component/repositorios.js
+++ src/component/repositorios.js
@@ -7,10 +7,21 @@ export default () => {
     const [repos, setRepos] = useState([]);
     
     useEffect(() => {
+
+        const data = sessionStorage.getItem("repos");
+        let myRepos;
+        if(data){
+            myRepos = JSON.parse(data);
+            return setRepos(myRepos);
+        }
+        
+
         async function fetchRepos() {
             const response = await fetch ("https://api.github.com/users/guerrasoto10/repos");
             let myRepos = await response.json();
             
+            sessionStorage.setItem("repos", JSON.stringify(myRepos));
+
             setRepos(myRepos);
         }
         fetchRepos();
